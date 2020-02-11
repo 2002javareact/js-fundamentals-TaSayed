a1abca774adec5901762b3cb7598adb134980003
tasayed <taqrim.sayed@gmail.com>
2020-02-11 12:59:04 -0500

    finished fib

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -2,5 +2,15 @@
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
 function fib(n) {
-
+    num1 = 1
+    num2 = 0
+    for(let i = 0;i<n; i++){
+        temp = num2
+        num2 = num1 + num2
+        num1 = temp
+    }
+    return num2
 }
+
+console.log(fib(7))
+//1 1 2 3 5 8 11 19
